adcda4e09417797f0d678b63d199130af5647626
PengPengQ <1747447034@qq.com>
2022-04-21 23:16:34 +0800

    style: change title logo color

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <title>碰碰漆的博客</title>
         <meta
           name="description"
-          content="Blog 博客 个人博客 PengPengQ 碰碰漆"
+          content="Blog 博客 个人博客 PengPengQ 碰碰漆 漆鹏的博客 碰碰漆的博客"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -19,7 +19,7 @@ export default function Home() {
         </h1>
         <article className="mt-5 table:text-1.1textxl sm:text-lg text-base table:w-590 sm:w-506 phone:w-450 w-380 mx-auto font-sans font-light text-gray-800 dark:text-gray-300">
           <p className="text-center">
-            I’m a web <strong>software engineer</strong> from Chengdu, China.
+            I’m a <strong>web software engineer</strong> from Chengdu, China.
             <br />I currently enjoymy last college days. Writing an excellent
             open source project is mygoal. So, I improve my skills everyday.{" "}
             <br />
